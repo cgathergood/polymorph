57980b5e6cc33888e19665a59482c73e4bc0f2b2
cgathergood <calumgathergood@gmail.com>
2019-06-02 13:18:03 +1000

    Fun with gradient backgrounds

--- app/src/main/java/com/gathergood/calum/polymorph/MainActivity.kt
+++ app/src/main/java/com/gathergood/calum/polymorph/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.gathergood.calum.polymorph
 
+import android.graphics.drawable.AnimationDrawable
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
-import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,6 +20,12 @@ class MainActivity : AppCompatActivity() {
         bottomNavigationView = findViewById(R.id.bottom_navigation)
 
         setupNavigation()
+
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+        val animDrawable = main_background.background as AnimationDrawable
+        animDrawable.setEnterFadeDuration(10)
+        animDrawable.setExitFadeDuration(3000)
+        animDrawable.start()
 
     }
 
